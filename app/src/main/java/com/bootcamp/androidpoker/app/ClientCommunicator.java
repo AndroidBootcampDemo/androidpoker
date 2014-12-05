ba0222e48068fbffcfd7a3ca238bc41b5607e870
Aleksei Zolotov <freopen@google.com>
2014-12-05 10:36:22 +0100

    Client parsing JSON added.

--- app/src/main/java/com/bootcamp/androidpoker/app/ClientCommunicator.java
+++ app/src/main/java/com/bootcamp/androidpoker/app/ClientCommunicator.java
@@ -1,8 +1,10 @@
 package com.bootcamp.androidpoker.app;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,42 +12,63 @@ import java.util.List;
  */
 public abstract class ClientCommunicator {
     public void call() {
-        JSONObject response = new JSONObject();
+        JSONObject message = new JSONObject();
         try {
-            response.put("action", "call");
+            message.put("action", "call");
         } catch (JSONException e) {}
-        sendJSON(response);
+        sendJSON(message);
     }
 
     public void raise(int amount) {
-        JSONObject response = new JSONObject();
+        JSONObject message = new JSONObject();
         try {
-            response.put("action", "raise");
-            response.put("amount", amount);
+            message.put("action", "raise");
+            message.put("amount", amount);
         } catch (JSONException e) {}
-        sendJSON(response);
+        sendJSON(message);
     }
 
     public void fold() {
-        JSONObject response = new JSONObject();
+        JSONObject message = new JSONObject();
         try {
-            response.put("action", "fold");
+            message.put("action", "fold");
         } catch (JSONException e) {}
-        sendJSON(response);
+        sendJSON(message);
 
     }
 
     public void check() {
-        JSONObject response = new JSONObject();
+        JSONObject message = new JSONObject();
         try {
-            response.put("action", "check");
+            message.put("action", "check");
         } catch (JSONException e) {}
-        sendJSON(response);
+        sendJSON(message);
 
     }
 
-    private void sendJSON(JSONObject response) {
+    private void sendJSON(JSONObject message) {
 
+    }
+    
+    private void parseJSON(JSONObject message) {
+        try {
+            final int type = message.getInt("message_type");
+            final JSONObject args = message.getJSONObject("args")
+            if (type == 0) {
+                onChangeCash(args.getInt("cash"));
+            } else if (type == 1) {
+                JSONArray cards = args.getJSONArray("cards");
+                onShowCards(cards.getString(0), cards.getString(1));
+            } else if (type == 2) {
+                OnUpdateBetInfo(args.getInt("min_bet"), args.getInt("current_bet"));
+                JSONArray actions = args.getJSONArray("allowed_actions");
+                ArrayList<String> list_actions = new ArrayList<String>();
+                for (int i = 0; i < actions.length(); ++i) {
+                    list_actions.add(actions.getString(i));
+                }
+                onEnableActions(list_actions);
+            }
+        } catch (JSONException e) {}
     }
 
     abstract void onShowCards(String first, String second);
